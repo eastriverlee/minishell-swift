d52c2500e4bb470cd49b6eea3cdd8e920a659234
eastriverlee <dan0720@naver.com>
2021-08-28 18:32:24 +0900

    multiple pipes

--- minishell.swift
+++ minishell.swift
@@ -1,6 +1,21 @@
 import Foundation
 import Darwin
 
+private var alternate = false
+private var _pipe: Pipe = Pipe()
+private var pipe_: Pipe = Pipe()
+
+var currentPipe: Pipe { alternate ? _pipe : pipe_ }
+var previousPipe: Pipe { alternate ? pipe_ : _pipe }
+func revivePreviousPipe() { if alternate { pipe_ = Pipe() } else { _pipe = Pipe() } }
+
+enum Pipekind {
+    case first
+    case middle
+    case last
+    case none
+}
+
 func eof() {
     print("exit")
     exit(0)
@@ -14,21 +29,87 @@ var environment: [String: String] {
     ProcessInfo.processInfo.environment
 }
 
-func execute(_ arguments: [String]) {
+func connectPipe(of kind: Pipekind, with process: Process) {
+    switch kind {
+        case .first:
+            process.standardOutput = currentPipe.fileHandleForWriting
+        case .middle:
+            process.standardInput = previousPipe.fileHandleForReading
+            process.standardOutput = currentPipe.fileHandleForWriting
+        case .last: 
+            process.standardInput = previousPipe.fileHandleForReading
+        default: break
+    }
+}
+
+func closePipe(of kind: Pipekind) {
+    switch kind {
+        case .first:
+            try! currentPipe.fileHandleForWriting.close()
+        case .middle:
+            try! previousPipe.fileHandleForReading.close()
+            revivePreviousPipe()
+            try! currentPipe.fileHandleForWriting.close()
+        case .last: 
+            try! previousPipe.fileHandleForReading.close()
+            revivePreviousPipe()
+        default: break
+    }
+}
+
+func execute(_ command: [String], pipe kind: Pipekind = .none) {
     let process = Process()
     var launchPath = ""
 
-    process.arguments = Array(arguments.dropFirst())
-    let subpaths = ["."] + (environment["PATH"]?.components(separatedBy: ":") ?? [])
+    process.arguments = Array(command.dropFirst())
+    let subpaths = ["."] + (environment["PATH"] ?? "").components(separatedBy: ":")
+    connectPipe(of: kind, with: process)
     for subpath in subpaths {
-        launchPath = subpath + "/" + arguments[0]
+        launchPath = subpath + "/" + command[0]
         let executableURL = URL(fileURLWithPath: launchPath)
         process.executableURL = executableURL
-        do { 
+        do {
             try process.run()
             process.waitUntilExit()
+            closePipe(of: kind)
+            alternate.toggle()
             return
         } catch { continue }
+    }
+    exit(-1)
+}
+
+extension Array where Element == String {
+    func separate(by separator: String) -> [[Element]] {
+        var chunks: [[Element]] = []
+        var elements: [Element] = []
+
+        for element in self {
+            if element != separator {
+                elements.append(element)
+            } else {
+                chunks.append(elements)
+                elements = []
+            }
+        }
+        chunks.append(elements)
+        return chunks
+    }
+}
+
+func run(_ commands: [[String]]) {
+    let count = commands.count
+
+    if count == 1 {
+        execute(commands[0])
+    } else {
+        for i in 0..<count {
+            switch i {
+                case 0: execute(commands[i], pipe: .first)
+                case count-1: execute(commands[i], pipe: .last)
+                default: execute(commands[i], pipe: .middle)
+            }
+        }
     }
 }
 
@@ -37,7 +118,8 @@ func main() {
         prompt()
         guard let line = readLine() else { return eof() }
         let arguments = line.components(separatedBy: " ").filter { $0 != "" } 
-        execute(arguments)
+        let commands = arguments.separate(by: "|")
+        run(commands)
     }
 }
 
